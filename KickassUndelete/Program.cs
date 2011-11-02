6eee71f7710c11afa029d51064a11ab720061c57
Lukas Korsika <lukas@korsika.co.nz>
2011-11-02 02:06:32 +0000

    Fixes ticket 2. We now try to relaunch the program as an Administrator if we don't have enough permissions (aka OBTAIN ALL THE PERMISSIONS).

--- KickassUndelete/Program.cs
+++ KickassUndelete/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Security.Principal;
+using System.Diagnostics;
 
 namespace KickassUndelete {
     static class Program {
@@ -10,9 +12,26 @@ namespace KickassUndelete {
         /// </summary>
         [STAThread]
         static void Main() {
+			EnsureUserIsAdmin();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
+
+		static void EnsureUserIsAdmin() {
+			WindowsPrincipal principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+			if (!principal.IsInRole(WindowsBuiltInRole.Administrator)) {
+				RelaunchAsAdmin();
+			}
+		}
+
+		static void RelaunchAsAdmin() {
+			ProcessStartInfo psi = new ProcessStartInfo(Application.ExecutablePath);
+			psi.Verb = "runas";
+			Process.Start(psi);
+
+			Environment.Exit(0);
+		}
     }
 }
