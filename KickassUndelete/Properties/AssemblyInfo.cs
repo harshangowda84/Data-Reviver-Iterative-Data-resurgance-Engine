f5e270f57cde0bd65eb3a174a81b65c0de59443b
Joey Scarr <joey.scarr+github@gmail.com>
2013-10-10 22:44:27 +1300

    Update assembly copyright.

--- KickassUndelete/Properties/AssemblyInfo.cs
+++ KickassUndelete/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Joey Scarr")]
 [assembly: AssemblyProduct("KickassUndelete")]
-[assembly: AssemblyCopyright("Copyright © Joey Scarr 2011")]
+[assembly: AssemblyCopyright("Copyright © Joey Scarr 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
