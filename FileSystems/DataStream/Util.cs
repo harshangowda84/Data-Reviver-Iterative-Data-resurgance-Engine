16b09317a81aefec3e8f3ca372f49b27201e7a28
joshoosterman <josh.oosterman@gmail.com>
2014-09-20 11:39:40 +1000

    Fix borked indentation. Still getting the hang of VS again.

--- FileSystems/DataStream/Util.cs
+++ FileSystems/DataStream/Util.cs
@@ -228,9 +228,9 @@ namespace KFS.DataStream {
 		}
 
 		public static string DetectFSType(IDataStream stream) {
-            if (GetASCIIString(stream, 3, 4) == "NTFS") {
-                return "NTFS";
-            }
+			if (GetASCIIString(stream, 3, 4) == "NTFS") {
+				return "NTFS";
+			}
 			if (GetASCIIString(stream, 0x36, 5) == "FAT16") {
 				return "FAT16";
 			}
